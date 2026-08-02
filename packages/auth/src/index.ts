704dea18ed299c78b0a036e1903bd7c11bc02b30
suwizx <suwijak.pak@gmail.com>
2026-08-02 14:45:12 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@singpore-game/db";
+import * as schema from "@singpore-game/db/schema/auth";
+import { env } from "@singpore-game/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
